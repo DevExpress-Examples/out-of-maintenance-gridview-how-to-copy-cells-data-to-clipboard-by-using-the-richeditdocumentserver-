d20701db0956cbc857cc51980f3a10b043956294
Pabloid11 <pavel.korovkin@gmail.com>
2020-04-09 18:00:41 +0300

    VB version of the example

--- VB/Q577904/CF_HTMLHelper.vb
+++ VB/Q577904/CF_HTMLHelper.vb
@@ -17,49 +17,49 @@ Imports DevExpress.XtraRichEdit.Commands.Internal
 Imports DevExpress.XtraRichEdit.Export.Html
 
 Namespace Q577904
-    Public Class CF_HTMLHelper
-        'HTML Clipboard Format http://msdn.microsoft.com/en-us/library/aa767917(v=vs.85).aspx
-        Private Const StartFragmentTag As String = "<!--StartFragment-->"
-        Private Const EndFragmentTag As String = "<!--EndFragment-->"
+	Public Class CF_HTMLHelper
+		'HTML Clipboard Format http://msdn.microsoft.com/en-us/library/aa767917(v=vs.85).aspx
+		Private Const StartFragmentTag As String = "<!--StartFragment-->"
+		Private Const EndFragmentTag As String = "<!--EndFragment-->"
 
-        Private Const bodyTag As String = "<body>" & ControlChars.CrLf
-        Private Const bodyTagClose As String = "</body>"
-        Private Const EmptyDescription As String = "Version:0.9" & ControlChars.CrLf & "StartHTML:{0:D10}" & ControlChars.CrLf & "EndHTML:{1:D10}" & ControlChars.CrLf & "StartFragment:{2:D10}" & ControlChars.CrLf & "EndFragment:{3:D10}" & ControlChars.CrLf
+		Private Const bodyTag As String = "<body>" & vbCrLf
+		Private Const bodyTagClose As String = "</body>"
+		Private Const EmptyDescription As String = "Version:0.9" & vbCrLf & "StartHTML:{0:D10}" & vbCrLf & "EndHTML:{1:D10}" & vbCrLf & "StartFragment:{2:D10}" & vbCrLf & "EndFragment:{3:D10}" & vbCrLf
 
-        Public Shared Function GetHtmlClipboardFormat(ByVal html As String) As String
-            Dim startBodyTagPos As Integer = html.IndexOf(bodyTag)
-            Dim bodyEndTagPos As Integer = html.LastIndexOf(bodyTagClose)
+		Public Shared Function GetHtmlClipboardFormat(ByVal html As String) As String
+			Dim startBodyTagPos As Integer = html.IndexOf(bodyTag)
+			Dim bodyEndTagPos As Integer = html.LastIndexOf(bodyTagClose)
 
-            Dim contentBeforeFramentLength As Integer = startBodyTagPos + bodyTag.Length
-            Dim contentBeforeFragment As String = html.Substring(0, contentBeforeFramentLength)
+			Dim contentBeforeFramentLength As Integer = startBodyTagPos + bodyTag.Length
+			Dim contentBeforeFragment As String = html.Substring(0, contentBeforeFramentLength)
 
-            Dim fragment As String = html.Substring(contentBeforeFramentLength, bodyEndTagPos - contentBeforeFramentLength)
+			Dim fragment As String = html.Substring(contentBeforeFramentLength, bodyEndTagPos - contentBeforeFramentLength)
 
-            Dim contentAfterFragment As String = html.Substring(bodyEndTagPos, html.Length - bodyEndTagPos)
+			Dim contentAfterFragment As String = html.Substring(bodyEndTagPos, html.Length - bodyEndTagPos)
 
-            Dim result As String = Get_CF_HTML(contentBeforeFragment & StartFragmentTag, fragment, EndFragmentTag & contentAfterFragment)
+			Dim result As String = Get_CF_HTML(contentBeforeFragment & StartFragmentTag, fragment, EndFragmentTag & contentAfterFragment)
 
-            Return result
-        End Function
+			Return result
+		End Function
 
-        Private Shared Function Get_CF_HTML(ByVal contentBeforeFragment As String, ByVal fragment As String, ByVal contentAfterFragment As String) As String
-            Dim contentBeforeFragmentCount As Integer = Encoding.UTF8.GetByteCount(contentBeforeFragment)
-            Dim fragmentCount As Integer = Encoding.UTF8.GetByteCount(fragment)
-            Dim contentAfterFragmentCount As Integer = Encoding.UTF8.GetByteCount(contentAfterFragment)
+		Private Shared Function Get_CF_HTML(ByVal contentBeforeFragment As String, ByVal fragment As String, ByVal contentAfterFragment As String) As String
+			Dim contentBeforeFragmentCount As Integer = Encoding.UTF8.GetByteCount(contentBeforeFragment)
+			Dim fragmentCount As Integer = Encoding.UTF8.GetByteCount(fragment)
+			Dim contentAfterFragmentCount As Integer = Encoding.UTF8.GetByteCount(contentAfterFragment)
 
-            Dim descriptionOffset As Integer = Encoding.UTF8.GetByteCount(String.Format(EmptyDescription, 0, 0, 0, 0))
-            Dim endHTMLOffset As Integer = descriptionOffset + contentBeforeFragmentCount + fragmentCount + contentAfterFragmentCount
-            Dim startFragmentOffset As Integer = descriptionOffset + contentBeforeFragmentCount
-            Dim endFragmentOffset As Integer = descriptionOffset + contentBeforeFragmentCount + fragmentCount
+			Dim descriptionOffset As Integer = Encoding.UTF8.GetByteCount(String.Format(EmptyDescription, 0, 0, 0, 0))
+			Dim endHTMLOffset As Integer = descriptionOffset + contentBeforeFragmentCount + fragmentCount + contentAfterFragmentCount
+			Dim startFragmentOffset As Integer = descriptionOffset + contentBeforeFragmentCount
+			Dim endFragmentOffset As Integer = descriptionOffset + contentBeforeFragmentCount + fragmentCount
 
-            Dim description As String = String.Format(EmptyDescription, descriptionOffset, endHTMLOffset, startFragmentOffset, endFragmentOffset)
+			Dim description As String = String.Format(EmptyDescription, descriptionOffset, endHTMLOffset, startFragmentOffset, endFragmentOffset)
 
-            Dim content As New StringBuilder()
-            content.Append(description)
-            content.Append(contentBeforeFragment)
-            content.Append(fragment)
-            content.Append(contentAfterFragment)
-            Return content.ToString()
-        End Function
-    End Class
+			Dim content As New StringBuilder()
+			content.Append(description)
+			content.Append(contentBeforeFragment)
+			content.Append(fragment)
+			content.Append(contentAfterFragment)
+			Return content.ToString()
+		End Function
+	End Class
 End Namespace
--- VB/Q577904/Form1.vb
+++ VB/Q577904/Form1.vb
@@ -17,125 +17,155 @@ Imports DevExpress.XtraRichEdit.Commands.Internal
 Imports DevExpress.XtraRichEdit.Export.Html
 
 Namespace Q577904
-    Partial Public Class Form1
-        Inherits Form
+	Partial Public Class Form1
+		Inherits Form
 
-        Public Sub New()
-            InitializeComponent()
-            gridControl1.DataSource = CreateTable(20)
-        End Sub
+		Public Sub New()
+			InitializeComponent()
+			gridControl1.DataSource = CreateTable(20)
+		End Sub
 
-        Private Function CreateTable(ByVal rowCount As Integer) As DataTable
-            Dim dataTable As New DataTable()
-            dataTable.Columns.Add("String", GetType(String))
-            dataTable.Columns.Add("Int", GetType(Integer))
-            dataTable.Columns.Add("Date", GetType(Date))
-            For i As Integer = 0 To rowCount - 1
-                dataTable.Rows.Add("Row" & i, i, Date.Today.AddDays(i))
-            Next i
-            Return dataTable
-        End Function
+		Private Function CreateTable(ByVal rowCount As Integer) As DataTable
+			Dim dataTable As New DataTable()
+			dataTable.Columns.Add("String", GetType(String))
+			dataTable.Columns.Add("Int", GetType(Integer))
+			dataTable.Columns.Add("Date", GetType(Date))
+			For i As Integer = 0 To rowCount - 1
+				dataTable.Rows.Add("Row" & i, i, Date.Today.AddDays(i))
+			Next i
+			Return dataTable
+		End Function
 
-        Private Sub gridControl1_KeyDown(ByVal sender As Object, ByVal e As KeyEventArgs) Handles gridControl1.KeyDown
-            If e.KeyData = (Keys.C Or Keys.Control) OrElse e.KeyData = (Keys.Insert Or Keys.Control) Then
-                CopyToClipboard()
-                e.Handled = True
-            End If
-        End Sub
+		Private Sub gridControl1_KeyDown(ByVal sender As Object, ByVal e As KeyEventArgs) Handles gridControl1.KeyDown
+			If e.KeyData = (Keys.C Or Keys.Control) OrElse e.KeyData = (Keys.Insert Or Keys.Control) Then
+				CopyToClipboard()
+				e.Handled = True
+			End If
+		End Sub
 
-        Private Sub CopyColumns(ByVal columns As IOrderedEnumerable(Of IGrouping(Of GridColumn, GridCell)), ByVal srv As RichEditDocumentServer, ByVal table As Table)
-            Dim i As Integer = 0
-            For Each column In columns
-                Dim cell As TableCell = GetCell(table, 0, i)
-                srv.Document.InsertText(cell.Range.Start, column.Key.GetTextCaption())
-                cell.BackgroundColor = Color.Gray
-                cell.PreferredWidthType = WidthType.Fixed
-                Using g As Graphics = Me.CreateGraphics()
-                    cell.PreferredWidth = Units.PixelsToDocumentsF(column.Key.VisibleWidth, g.DpiX)
-                End Using
-                i += 1
-            Next column
-        End Sub
+		Private Sub CopyColumns(ByVal columns As IOrderedEnumerable(Of IGrouping(Of GridColumn, GridCell)), ByVal srv As RichEditDocumentServer, ByVal table As Table)
+			Dim i As Integer = 0
+			For Each column In columns
+				Dim cell As TableCell = GetCell(table, 0, i)
+				srv.Document.InsertText(cell.Range.Start, column.Key.GetTextCaption())
+				cell.BackgroundColor = Color.Gray
+				cell.PreferredWidthType = WidthType.Fixed
+				Using g As Graphics = Me.CreateGraphics()
+					cell.PreferredWidth = Units.PixelsToDocumentsF(column.Key.VisibleWidth, g.DpiX)
+				End Using
+				i += 1
+			Next column
+		End Sub
 
-        Private Shared Function GetCell(ByVal table As Table, ByVal i As Integer, ByVal j As Integer) As TableCell
-            Dim cell As TableCell = table.Cell(i, j)
-            Return cell
-        End Function
+		Private Shared Function GetCell(ByVal table As Table, ByVal i As Integer, ByVal j As Integer) As TableCell
+			Dim cell As TableCell = table.Cell(i, j)
+			Return cell
+		End Function
 
-        Private Sub CopyCells(ByVal rows As IOrderedEnumerable(Of IGrouping(Of Int32, GridCell)), ByVal srv As RichEditDocumentServer, ByVal table As Table)
-            Dim viewInfo As GridViewInfo = CType(gridView1.GetViewInfo(), GridViewInfo)
-            Dim i As Integer = 1
-            For Each row In rows
-                row.OrderBy(Function(z) z.Column.VisibleIndex)
-                Dim j As Integer = 0
-                For Each c In row
-                    Dim cell As TableCell = GetCell(table, i, j)
-                    srv.Document.InsertText(cell.Range.Start, gridView1.GetRowCellDisplayText(c.RowHandle, c.Column))
-                    Dim gridCellInfo As GridCellInfo = GetGridCellInfo(viewInfo, c)
-                    cell.BackgroundColor = gridCellInfo.Appearance.BackColor
-                    j += 1
-                Next c
-                i += 1
-            Next row
-        End Sub
+		Private Sub CopyCells(ByVal rows As IOrderedEnumerable(Of IGrouping(Of Int32, GridCell)), ByVal srv As RichEditDocumentServer, ByVal table As Table)
+			Dim viewInfo As GridViewInfo = CType(gridView1.GetViewInfo(), GridViewInfo)
+			Dim i As Integer = 1
+			For Each row In rows
+				row.OrderBy(Function(z) z.Column.VisibleIndex)
+				Dim j As Integer = 0
+				For Each c In row
+					Dim cell As TableCell = GetCell(table, i, j)
+					srv.Document.InsertText(cell.Range.Start, gridView1.GetRowCellDisplayText(c.RowHandle, c.Column))
+					Dim gridCellInfo As GridCellInfo = GetGridCellInfo(viewInfo, c)
+					cell.BackgroundColor = gridCellInfo.Appearance.BackColor
+					j += 1
+				Next c
+				i += 1
+			Next row
+		End Sub
 
-        Private Sub SetBorders(ByVal table As Table)
-            Dim thickness As Single = 1.0F
-            table.Borders.InsideVerticalBorder.LineColor = gridView1.PaintAppearance.VertLine.BackColor
-            table.Borders.Right.LineColor = table.Borders.InsideVerticalBorder.LineColor
-            table.Borders.Left.LineColor = table.Borders.Right.LineColor
-            table.Borders.InsideVerticalBorder.LineThickness = thickness
-            table.Borders.Right.LineThickness = table.Borders.InsideVerticalBorder.LineThickness
-            table.Borders.Left.LineThickness = table.Borders.Right.LineThickness
-            table.Borders.InsideVerticalBorder.LineStyle = TableBorderLineStyle.Single
-            table.Borders.Right.LineStyle = table.Borders.InsideVerticalBorder.LineStyle
-            table.Borders.Left.LineStyle = table.Borders.Right.LineStyle
-            table.Borders.InsideHorizontalBorder.LineColor = gridView1.PaintAppearance.HorzLine.BackColor
-            table.Borders.Top.LineColor = table.Borders.InsideHorizontalBorder.LineColor
-            table.Borders.Bottom.LineColor = table.Borders.Top.LineColor
-            table.Borders.InsideHorizontalBorder.LineThickness = thickness
-            table.Borders.Top.LineThickness = table.Borders.InsideHorizontalBorder.LineThickness
-            table.Borders.Bottom.LineThickness = table.Borders.Top.LineThickness
-            table.Borders.InsideHorizontalBorder.LineStyle = TableBorderLineStyle.Single
-            table.Borders.Top.LineStyle = table.Borders.InsideHorizontalBorder.LineStyle
-            table.Borders.Bottom.LineStyle = table.Borders.Top.LineStyle
-        End Sub
+		Private Sub SetBorders(ByVal table As Table)
+			Dim thickness As Single = 1F
+'INSTANT VB WARNING: An assignment within expression was extracted from the following statement:
+'ORIGINAL LINE: table.Borders.Left.LineColor = table.Borders.Right.LineColor = table.Borders.InsideVerticalBorder.LineColor = gridView1.PaintAppearance.VertLine.BackColor;
+			table.Borders.InsideVerticalBorder.LineColor = gridView1.PaintAppearance.VertLine.BackColor
+'INSTANT VB WARNING: An assignment within expression was extracted from the following statement:
+'ORIGINAL LINE: table.Borders.Left.LineColor = table.Borders.Right.LineColor = table.Borders.InsideVerticalBorder.LineColor
+			table.Borders.Right.LineColor = table.Borders.InsideVerticalBorder.LineColor
+			table.Borders.Left.LineColor = table.Borders.Right.LineColor
+'INSTANT VB WARNING: An assignment within expression was extracted from the following statement:
+'ORIGINAL LINE: table.Borders.Left.LineThickness = table.Borders.Right.LineThickness = table.Borders.InsideVerticalBorder.LineThickness = thickness;
+			table.Borders.InsideVerticalBorder.LineThickness = thickness
+'INSTANT VB WARNING: An assignment within expression was extracted from the following statement:
+'ORIGINAL LINE: table.Borders.Left.LineThickness = table.Borders.Right.LineThickness = table.Borders.InsideVerticalBorder.LineThickness
+			table.Borders.Right.LineThickness = table.Borders.InsideVerticalBorder.LineThickness
+			table.Borders.Left.LineThickness = table.Borders.Right.LineThickness
+'INSTANT VB WARNING: An assignment within expression was extracted from the following statement:
+'ORIGINAL LINE: table.Borders.Left.LineStyle = table.Borders.Right.LineStyle = table.Borders.InsideVerticalBorder.LineStyle = TableBorderLineStyle.Single;
+			table.Borders.InsideVerticalBorder.LineStyle = TableBorderLineStyle.Single
+'INSTANT VB WARNING: An assignment within expression was extracted from the following statement:
+'ORIGINAL LINE: table.Borders.Left.LineStyle = table.Borders.Right.LineStyle = table.Borders.InsideVerticalBorder.LineStyle
+			table.Borders.Right.LineStyle = table.Borders.InsideVerticalBorder.LineStyle
+			table.Borders.Left.LineStyle = table.Borders.Right.LineStyle
+'INSTANT VB WARNING: An assignment within expression was extracted from the following statement:
+'ORIGINAL LINE: table.Borders.Bottom.LineColor = table.Borders.Top.LineColor = table.Borders.InsideHorizontalBorder.LineColor = gridView1.PaintAppearance.HorzLine.BackColor;
+			table.Borders.InsideHorizontalBorder.LineColor = gridView1.PaintAppearance.HorzLine.BackColor
+'INSTANT VB WARNING: An assignment within expression was extracted from the following statement:
+'ORIGINAL LINE: table.Borders.Bottom.LineColor = table.Borders.Top.LineColor = table.Borders.InsideHorizontalBorder.LineColor
+			table.Borders.Top.LineColor = table.Borders.InsideHorizontalBorder.LineColor
+			table.Borders.Bottom.LineColor = table.Borders.Top.LineColor
+'INSTANT VB WARNING: An assignment within expression was extracted from the following statement:
+'ORIGINAL LINE: table.Borders.Bottom.LineThickness = table.Borders.Top.LineThickness = table.Borders.InsideHorizontalBorder.LineThickness = thickness;
+			table.Borders.InsideHorizontalBorder.LineThickness = thickness
+'INSTANT VB WARNING: An assignment within expression was extracted from the following statement:
+'ORIGINAL LINE: table.Borders.Bottom.LineThickness = table.Borders.Top.LineThickness = table.Borders.InsideHorizontalBorder.LineThickness
+			table.Borders.Top.LineThickness = table.Borders.InsideHorizontalBorder.LineThickness
+			table.Borders.Bottom.LineThickness = table.Borders.Top.LineThickness
+'INSTANT VB WARNING: An assignment within expression was extracted from the following statement:
+'ORIGINAL LINE: table.Borders.Bottom.LineStyle = table.Borders.Top.LineStyle = table.Borders.InsideHorizontalBorder.LineStyle = TableBorderLineStyle.Single;
+			table.Borders.InsideHorizontalBorder.LineStyle = TableBorderLineStyle.Single
+'INSTANT VB WARNING: An assignment within expression was extracted from the following statement:
+'ORIGINAL LINE: table.Borders.Bottom.LineStyle = table.Borders.Top.LineStyle = table.Borders.InsideHorizontalBorder.LineStyle
+			table.Borders.Top.LineStyle = table.Borders.InsideHorizontalBorder.LineStyle
+			table.Borders.Bottom.LineStyle = table.Borders.Top.LineStyle
+		End Sub
 
-        Private Sub CopyToClipboard()
-            Dim cells() As GridCell = gridView1.GetSelectedCells()
-            Dim rows = cells.GroupBy(Function(c) c.RowHandle).Select(Function(gr) gr).OrderBy(Function(gr) gr.Key)
-            Dim columns = cells.GroupBy(Function(c) c.Column).Select(Function(gr) gr).OrderBy(Function(gr) gr.Key.VisibleIndex)
-            Dim srv As New RichEditDocumentServer()
-            srv.CreateNewDocument()
-            Dim table As Table = srv.Document.InsertTable(srv.Document.CaretPosition, rows.Count() + 1, columns.Count())
-            SetBorders(table)
-            CopyColumns(columns, srv, table)
-            CopyCells(rows, srv, table)
+		Private Sub CopyToClipboard()
+			Dim cells() As GridCell = gridView1.GetSelectedCells()
+			Dim rows = From c In cells
+				Group c By c.RowHandle Into gr = Group
+				Order By RowHandle
+				Select gr
+			Dim columns = From c In cells
+				Group c By c.Column Into gr = Group
+				Order By Column.VisibleIndex
+				Select gr
+			Dim srv As New RichEditDocumentServer()
+			srv.CreateNewDocument()
+			Dim table As Table = srv.Document.Tables.Create(srv.Document.CaretPosition, rows.Count() + 1, columns.Count())
+			SetBorders(table)
+			CopyColumns(columns, srv, table)
+			CopyCells(rows, srv, table)
 
-            Dim options As DevExpress.XtraRichEdit.Export.HtmlDocumentExporterOptions = srv.Options.Export.Html
-            options.ExportRootTag = ExportRootTag.Html
-            options.Encoding = Encoding.UTF8
-            options.CssPropertiesExportType = CssPropertiesExportType.Inline
-            options.UriExportType = UriExportType.Absolute
-            options.EmbedImages = False
+			Dim options As DevExpress.XtraRichEdit.Export.HtmlDocumentExporterOptions = srv.Options.Export.Html
+			options.ExportRootTag = ExportRootTag.Html
+			options.Encoding = Encoding.UTF8
+			options.CssPropertiesExportType = CssPropertiesExportType.Inline
+			options.UriExportType = UriExportType.Absolute
+			options.EmbedImages = False
 
-            Dim htmlContent As String = srv.HtmlText
-            Dim cfHtml As String = CF_HTMLHelper.GetHtmlClipboardFormat(htmlContent)
+			Dim htmlContent As String = srv.HtmlText
+			Dim cfHtml As String = CF_HTMLHelper.GetHtmlClipboardFormat(htmlContent)
 
-            Dim dataObject As IDataObject = New DataObject()
-            dataObject.SetData(DataFormats.Text, srv.Text)
-            dataObject.SetData(DataFormats.Html, cfHtml)
-            Clipboard.SetDataObject(dataObject, True)
+			Dim dataObject As IDataObject = New DataObject()
+			dataObject.SetData(DataFormats.Text, srv.Text)
+			dataObject.SetData(DataFormats.Html, cfHtml)
+			Clipboard.SetDataObject(dataObject, True)
 
-        End Sub
+		End Sub
 
-        Private Function GetGridCellInfo(ByVal viewInfo As GridViewInfo, ByVal cell As GridCell) As GridCellInfo
-            Dim gridCellInfo As GridCellInfo = viewInfo.GetGridCellInfo(cell.RowHandle, cell.Column)
-            gridCellInfo.State = gridCellInfo.State And Not (GridRowCellState.Focused Or GridRowCellState.FocusedCell Or GridRowCellState.Selected)
-            Dim method As System.Reflection.MethodInfo = viewInfo.GetType().GetMethod("UpdateCellAppearanceCore", System.Reflection.BindingFlags.Instance Or System.Reflection.BindingFlags.NonPublic)
-            method.Invoke(viewInfo, New Object() {gridCellInfo, True, True, Nothing})
-            Return gridCellInfo
-        End Function
-    End Class
+		Private Function GetGridCellInfo(ByVal viewInfo As GridViewInfo, ByVal cell As GridCell) As GridCellInfo
+			Dim gridCellInfo As GridCellInfo = viewInfo.GetGridCellInfo(cell.RowHandle, cell.Column)
+			gridCellInfo.State = gridCellInfo.State And Not (GridRowCellState.Focused Or GridRowCellState.FocusedCell Or GridRowCellState.Selected)
+			Dim method As System.Reflection.MethodInfo = viewInfo.GetType().GetMethod("UpdateCellAppearanceCore", System.Reflection.BindingFlags.Instance Or System.Reflection.BindingFlags.NonPublic)
+			method.Invoke(viewInfo, New Object() { gridCellInfo, True, True, Nothing })
+			Return gridCellInfo
+		End Function
+	End Class
 
 End Namespace
